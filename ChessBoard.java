753f8f6ca54571a00c6fd356f8bdae41ab43bca0
who-stole-my-username <exeteusog@gmail.com>
2025-06-14 12:20:09 +0200

    Progress

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,18 +1,16 @@
 package Chess;
 
-public class ChessBoard {
-    public class StartMenu {
-        public static void main(String[] args) {
+import java.util.Scanner;
 
-        }
-    }
+public class ChessBoard {
 
     public class Board {
-        String[][] board = new String[8][8]; 
+        String[][] board = new String[8][8];
+
         public Board() {
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 8; j++) {
-                    board[i][j] = " "; 
+                    board[i][j] = " ";
                 }
             }
         }
@@ -20,14 +18,20 @@ public class ChessBoard {
         public void printBoard() {
             System.out.println("+---+---+---+---+---+---+---+---+");
             for (int i = 0; i < 8; i++) {
-                System.out.print("| ");
+                System.out.print("|");
                 for (int j = 0; j < 8; j++) {
-                    System.out.print(board[i][j] + " | ");
+                    System.out.print(" " + board[i][j] + " |");
                 }
                 System.out.println();
                 System.out.println("+---+---+---+---+---+---+---+---+");
             }
         }
+    }
+
+    public static void game() {
+        ChessBoard gameInstance = new ChessBoard();
+        ChessBoard.Board myBoard = gameInstance.new Board();
+        myBoard.printBoard();
     }
 
     public void black() {
@@ -40,11 +44,15 @@ public class ChessBoard {
 
     public static void main(String[] args) {
         System.out.println("+-------------------------+");
-        System.out.println("|          Hello          |");
+        System.out.println("|   Type \"Start\" to play  |");
         System.out.println("+-------------------------+");
-        ChessBoard game = new ChessBoard(); 
-        ChessBoard.Board myBoard = game.new Board();
+        System.out.print("[Player_1]$ ");
+        Scanner scnr = new Scanner(System.in);
+        String inStart = scnr.nextLine();
 
-        myBoard.printBoard();
+        if (inStart.equals("Start")) {
+            game();
+        }
+        scnr.close();
     }
 }
