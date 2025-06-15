b25fdd118d5b76140fe38049c63c14c9a776643c
who-stole-my-username <exeteusog@gmail.com>
2025-06-15 09:14:40 +0200

    Progress

--- ChessBoard.java
+++ ChessBoard.java
@@ -13,9 +13,35 @@ public class ChessBoard {
                     board[i][j] = " ";
                 }
             }
+        
+
+            board[0][0] = "BR";
+            board[0][1] = "BN";
+            board[0][2] = "BB";
+            board[0][3] = "BQ";
+            board[0][4] = "BK";
+            board[0][5] = "BB";
+            board[0][6] = "BN";
+            board[0][7] = "BR";
+            for (int i = 0; i < 8; i++) {
+                board[1][i] = "BP";
+            }
+ 
+            board[1][0] = "WR";
+            board[1][1] = "WN";
+            board[1][2] = "WB";
+            board[1][3] = "WQ";
+            board[1][4] = "WK";
+            board[1][5] = "WB";
+            board[1][6] = "WN";
+            board[1][7] = "WR";
+            for (int i = 0; i < 8; i++) {
+                board[6][i] = "WP";
+            }
         }
 
-        public void printBoard() {
+        public void prBoard() {
+            System.out.println("  A   B   C   D   E   F   G   H");
             System.out.println("+---+---+---+---+---+---+---+---+");
             for (int i = 0; i < 8; i++) {
                 System.out.print("|");
@@ -31,7 +57,7 @@ public class ChessBoard {
     public static void game() {
         ChessBoard gameInstance = new ChessBoard();
         ChessBoard.Board myBoard = gameInstance.new Board();
-        myBoard.printBoard();
+        myBoard.prBoard();
     }
 
     public void black() {
