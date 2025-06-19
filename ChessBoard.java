a4de54178f147f308bd6b140ba26dd3ea60ebb72
who-stole-my-username <exeteusog@gmail.com>
2025-06-19 09:15:01 +0200

    Removed some errors and added the rest of the comments

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,4 +1,4 @@
-package Chess;
+package Chess; //Das Packet des Schachspiels
 
 import java.util.Scanner; //importieren des Scanners
 
@@ -311,39 +311,39 @@ public class ChessBoard { //Die Klasse in der alles definiert wird, was mit der 
     public static boolean ISA(int row, int col, String aColor, Board board) { //Diese Methode überprüft, ob ein bestimmtes Feld von einer gegnerischen Figur angegriffem wird
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                Pieces atk = board.board[i][j];
-                if (!atk.gType().equals(Pieces.Empty) && atk.gColor().equals(aColor)) { //Wenn das Feld eine Figur enthält und ich gang go schloofe
-                    Pieces oTar = board.board[row][col];
-                    board.board[row][col] = new Pieces(Pieces.Empty, Pieces.None);
+                Pieces atk = board.board[i][j]; //Alle Felder werden abgescannt
+                if (!atk.gType().equals(Pieces.Empty) && atk.gColor().equals(aColor)) { //Wenn das Feld eine eingene Figur enthält, und es unter attacke steht
+                    Pieces oTar = board.board[row][col]; //Speichert die Figur auf dem attackierten Feld für später
+                    board.board[row][col] = new Pieces(Pieces.Empty, Pieces.None); //Die Figur auf dem attackierten Feld wird weggenommen, um zu testen ob das Feld für die angreifende Figur theoretisch erreichbar ist
 
-                    boolean cAtk = false;
-                    switch (atk.gType()) {
-                        case Pieces.Pawn:
-                            int forwardDirection = (atk.gColor().equals(Pieces.White)) ? -1 : 1;
-                            if (Math.abs(col - j) == 1 && row == i + forwardDirection) {
+                    boolean cAtk = false; //Boolean, ob ein Feld Attackiert werden kann und nicht
+                    switch (atk.gType()) { 
+                        case Pieces.Pawn: //Falls die Angreifende Figur des Gegners ein Pawn ist
+                            int fD = (atk.gColor().equals(Pieces.White)) ? -1 : 1;
+                            if ((Math.abs(col - j)) == 1 && row == i + fD) { //Steht der Pawn diagonal zum Zielfeld? Wir haben herausgefunden das es Math.abs Methode gibt
                                 cAtk = true;
                             }
                             break;
-                        case Pieces.Rook:
+                        case Pieces.Rook: //Falls die angreifende Figur des Gegners ein Rook ist
                             cAtk = mRook(i, j, row, col, board, atk);
                             break;
-                        case Pieces.Knight:
+                        case Pieces.Knight: //Falls die angreifende Figur des Gegners ein Knight ist
                             cAtk = mKnight(i, j, row, col, board, atk);
                             break;
-                        case Pieces.Bishop:
+                        case Pieces.Bishop: //Falls die angreifende Figur des Gegners ein Bishop ist
                             cAtk = mBishop(i, j, row, col, board, atk);
                             break;
-                        case Pieces.Queen:
+                        case Pieces.Queen: //Falls die angreifende Figur des Gegners ein Queen ist
                             cAtk = mQueen(i, j, row, col, board, atk);
                             break;
-                        case Pieces.King:
-                            boolean someRow = (row == i || row == i + 1 || row == i - 1);
-                            boolean someCol = (col == j || col == j + 1 || col == j - 1);
-                            cAtk = someRow && someCol;
+                        case Pieces.King: //Falls die angreifende Figur des Gegners ein King ist 
+                            boolean someRow = (row == i || row == i + 1 || row == i - 1); //Ist in der selben Row oder ein Feld entfernt
+                            boolean someCol = (col == j || col == j + 1 || col == j - 1); //Ist in der selben Column oder ein Fed entfernt
+                            cAtk = someRow && someCol; //Column und Row müssen valid sein
                             break;
                     }
 
-                    board.board[row][col] = oTar;
+                    board.board[row][col] = oTar; //Die vorhin weggenommene Figur wird wieder platziert
 
                     if (cAtk) {
                         return true;
@@ -354,24 +354,24 @@ public class ChessBoard { //Die Klasse in der alles definiert wird, was mit der 
         return false;
     }
 
-    public static boolean ICKing(String kColor, Board board) {
-        int[] kingPos = lKing(kColor, board);
+    public static boolean ICKing(String kColor, Board board) { //Prüft, ob der König im Schach steht
+        int[] kingPos = lKing(kColor, board); //Findet die Position des Königs
         if (kingPos == null) {
             return false;
         }
 
-        String opColor = kColor.equals(Pieces.White) ? Pieces.Black : Pieces.White;
-        return ISA(kingPos[0], kingPos[1], opColor, board);
+        String opColor = kColor.equals(Pieces.White) ? Pieces.Black : Pieces.White; //Schaut, was die Farbe des Gegners ist
+        return ISA(kingPos[0], kingPos[1], opColor, board); //Gibt die Parameter an ISA weiter, um zu schauen ob der König im Schach stehen
     }
 
-    public static boolean IVM(int sRow, int sCol, int eRow, int eCol, Board board) {
-        Pieces piece = board.board[sRow][sCol];
+    public static boolean IVM(int sRow, int sCol, int eRow, int eCol, Board board) { //Schaut nur ob die Figur gültig ist und nicht, ob es im Schach selber gültig ist. Also ohne Schachregeln
+        Pieces piece = board.board[sRow][sCol]; //Definiert Piece
 
-        if (!IVMB(sRow, sCol, eRow, eCol, board, piece)) {
+        if (!IVMB(sRow, sCol, eRow, eCol, board, piece)) { //Falls IVMB nicht erfüllt ist automatisch schon falsch
             return false;
         }
 
-        switch (piece.gType()) {
+        switch (piece.gType()) { //Prüft alle Figuren durch
             case Pieces.Pawn:
                 return mPawn(sRow, sCol, eRow, eCol, board, piece);
             case Pieces.Rook:
@@ -390,90 +390,80 @@ public class ChessBoard { //Die Klasse in der alles definiert wird, was mit der 
         }
     }
 
-    public static void game() {
+    public static void game() { //Der Ablauf des Schachspieles als Methode
 
-        Scanner scnr = new Scanner(System.in);
-        Board board = new Board();
+        Scanner scnr = new Scanner(System.in); //Neues Scanner-objekt
+        Board board = new Board(); //Neues Board-objekt
 
         while (true) {
-            board.prBoard();
-            System.out.println("+-------------------------+");
+            board.prBoard(); //Zeichnet das Spielbrett
+            System.out.println("+-------------------------+"); //Die Zug-optionen
             System.out.println("| Enter your desired turn. |");
             System.out.println("|      Example: g2 f2      |");
             System.out.println("|      Don't type: :q      |");
             System.out.println("|    Or maybe do it :-)    |");
             System.out.println("+-------------------------+");
-            System.out.print("[" + (turn.equals(Pieces.White) ? "Player_1" : "Player_2") + "]$ ");
-            String move = scnr.nextLine();
+            System.out.print("[" + (turn.equals(Pieces.White) ? "Player_1" : "Player_2") + "]$ "); //Damit nicht steht "Weiss ist an der Reihe" haben wir hinzugefügt das dort ein Benutzername steht
+            String move = scnr.nextLine(); //Input des Spielers in move speichert
 
-            if (move.equals(":q")) {
+            if (move.equals(":q")) { //Input um das Spiel zu beenden
                 System.out.println("You either don't know what vim is or you just don't use it!");
                 System.out.println("So you should definitely try vim or neovim!");
                 break;
             }
 
-            String[] input = move.split(" ");
-            if (input.length != 2) {
-                System.out.println("Not a valid move!");
+            String[] input = move.split(" "); //Speichert den Input des Spielers getrennt in einem Array
+            if (input.length != 2) { 
+                System.out.println("Not a valid move!"); //Falls der Input nicht 2 Koordinaten enthält, wird wiederholt
                 continue;
             }
 
-            int[] fInput = pCords(input[0]);
-            int[] sInput = pCords(input[1]);
+            int[] fInput = pCords(input[0]); //Die Row Koordinate
+            int[] sInput = pCords(input[1]); //Die Col Koordinate
 
             if (fInput == null || sInput == null) {
-                System.out.println("Not valid coordinates!");
+                System.out.println("Not valid coordinates!"); //Wenn es Koordinaten sind die nicht auf dem Spielbrett sind, wird wiederholt
                 continue;
             }
 
-            int sRow = fInput[0];
-            int sCol = fInput[1];
-            int eRow = sInput[0];
-            int eCol = sInput[1];
+            int sRow = fInput[0]; //Die Row Koordinate vor dem Zug
+            int sCol = fInput[1]; //Die Column Koordinate vor dem zug
+            int eRow = sInput[0]; //Die Row Koordinate nach dem Zug
+            int eCol = sInput[1]; //Die Column Koordinate nach dem Zug
 
-            Pieces mpiece = board.board[sRow][sCol];
+            Pieces mpiece = board.board[sRow][sCol]; //Die Start-Koordinaten des gewünsten Zuges
 
-            if (mpiece.gType().equals(Pieces.Empty) || !mpiece.gColor().equals(turn)) {
-                System.out.println("Not a valid move or it's just not your turn!");
+            if (mpiece.gType().equals(Pieces.Empty))) {
+                System.out.println("Not a valid move, there is no piece on that field!"); //Falls es keine Figur auf dem gewünsten Feld hat, wird wiederholt
                 continue;
             }
 
-            if (!mpiece.gColor().equals(turn)) {
-                System.out.println("Not your turn, dear Mr. not neovim user.");
+            if (!mpiece.gColor().equals(turn)) { 
+                System.out.println("Not your turn, dear Mr. not neovim user."); //Falls der Spieler versucht, die Figuren des Gegners zu bewegen, wird wiederholt
                 continue;
             }
 
-            if (IVM(sRow, sCol, eRow, eCol, board)) {
-                board.board[eRow][eCol] = mpiece;
-                board.board[sRow][sCol] = new Pieces(Pieces.Empty, Pieces.None);
-                Turn();
+            if (IVMB(sRow, sCol, eRow, eCol, board, mpiece)) { //Prüfen, ob der Zug gültig ist
+                board.board[eRow][eCol] = mpiece; //Führt den erwünschten Zug aus, wenn er gültig ist
+                board.board[sRow][sCol] = new Pieces(Pieces.Empty, Pieces.None); //Entfernt die Figur auf dem ursrpünglichen Feld
+                Turn(); //Wechselt den Spieler, der am Zug ist
             } else {
             }
-
-            Turn();
         }
 
-        scnr.close();
+        scnr.close(); //Scanner wird geschlossen
     }
 
-    public void black() {
-
-    }
-
-    public void white() {
-
-    }
-
-    public static void main(String[] args) {
-        System.out.println("+--------------------------+");
+    public static void main(String[] args) { //Main Methode
+        System.out.println("+--------------------------+"); //Startmenu
         System.out.println("|   Type \"Start\" to play.  |");
         System.out.println("+--------------------------+");
-        Scanner scnr = new Scanner(System.in);
+        Scanner scnr = new Scanner(System.in); //Neuer Scanner
         String inStart = scnr.nextLine();
 
-        if (inStart.equals("Start")) {
-            game();
+        if (inStart.equals("Start")) { //Der Spieler muss Start schreiben
+            game(); //Dann wird Das ganze spiel ausgeführt
         }
-        scnr.close();
+        scnr.close(); //Scanner wird geschlossen
     }
 }
